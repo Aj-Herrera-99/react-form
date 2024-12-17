2fb79ea84b6961a488d102f68c42fc5a2f521a4a
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-17 14:34:27 +0100

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // states
+    const [formData, setFormData] = useState({ title: "" });
+    const [articles, setArticles] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // actions
+    const handleTitleChange = (e) => {
+        console.log(e.target.value)
+        setFormData({ ...formData, title: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("test");
+        setArticles([...articles, formData])
+    }
+
+    return (
+        <>
+            <header className="bg-red-300 p-5 ">
+                <h1 className="text-4xl">React Blog Form</h1>
+            </header>
+
+            <main className="bg-green-300 p-5">
+                <form action="#" onSubmit={handleSubmit}>
+                    <div className="flex flex-col items-start">
+                        <label htmlFor="title">Inserisci titolo articolo</label>
+                        <input
+                            className="border-2 border-blue-600 bg-stone-200"
+                            type="text"
+                            id="title"
+                            name="title"
+                            value={formData.title}
+                            onChange={handleTitleChange}
+                        />
+                    </div>
+                </form>
+                <ul className="flex flex-col gap-2 my-4 w-1/2">
+                    {articles.map((article, index) => (
+                        <li key={index} className="py-2 px-4 border-2 border-slate-400 rounded-md">Titolo: {article.title}</li>
+                    ))}
+                </ul>
+            </main>
+        </>
+    );
 }
 
-export default App
+export default App;
