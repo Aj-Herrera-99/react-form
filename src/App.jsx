005a3f0991756fc14f6788ebf4fb76f99584578b
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-17 15:19:54 +0100

    aggiunto campo autore

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,26 @@ import { useState } from "react";
 
 function App() {
     // states
-    const [formData, setFormData] = useState({ title: "" });
+    const [formData, setFormData] = useState({ title: "", author: "" });
     const [articles, setArticles] = useState([]);
 
     // actions
     const handleTitleChange = (e) => {
-        console.log(e.target.value);
         setFormData({ ...formData, title: e.target.value });
+    };
+    const handleAuthorChange = (e) => {
+        setFormData({ ...formData, author: e.target.value });
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("test");
+        console.log("Test");
         setArticles([...articles, formData]);
-        setFormData({ title: "" });
+        setFormData({ title: "", author: "" });
     };
 
     const handleRemoveClick = (e) => {
         const indexTarget = e.target.closest("li").getAttribute("index");
-        console.log(indexTarget);
         const filteredArticles = articles.filter(
             (article, index) => index != indexTarget
         );
@@ -34,10 +35,14 @@ function App() {
             </header>
 
             <main className="p-5 bg-green-300">
-                <form action="#" onSubmit={handleSubmit}>
+                <form
+                    action="#"
+                    onSubmit={handleSubmit}
+                    className="flex flex-col gap-4 max-w-[500px]"
+                >
                     <div className="flex flex-col items-start">
                         <label htmlFor="title" className="text-xl">
-                            Inserisci titolo articolo
+                            Inserisci titolo
                         </label>
                         <input
                             className="p-1 border-2 border-blue-600 rounded-md bg-stone-200"
@@ -46,29 +51,52 @@ function App() {
                             name="title"
                             value={formData.title}
                             onChange={handleTitleChange}
+                            required
                         />
                     </div>
+                    <div className="flex flex-col items-start">
+                        <label htmlFor="author" className="text-xl">
+                            Inserisci autore
+                        </label>
+                        <input
+                            className="p-1 border-2 border-blue-600 rounded-md bg-stone-200"
+                            type="text"
+                            id="author"
+                            name="author"
+                            value={formData.author}
+                            onChange={handleAuthorChange}
+                            required
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="py-2 text-white bg-blue-500 rounded-2xl hover:bg-blue-600"
+                    >
+                        Invia
+                    </button>
                 </form>
                 <ul className="flex flex-col w-1/2 gap-2 my-4 ">
                     {articles.map((article, index) => (
                         <li
                             key={index}
                             index={index}
-                            className="relative max-w-[500px]"
+                            className="relative max-w-[500px] px-4 py-2 border-2 rounded-md border-slate-400"
                         >
-                            <div>
-                                <div className="px-4 py-2 border-2 rounded-md border-slate-400">
-                                    <span className="font-semibold">
-                                        Titolo:
-                                    </span>
-                                    <span> {article.title}</span>
-                                </div>
-                                <span
-                                    onClick={handleRemoveClick}
-                                    className="absolute top-[-10px] right-0 text-xl text-red-600 pt-1 pr-1 cursor-pointer hover:scale-125"
-                                >
-                                    x
+                            <div className="flex flex-col">
+                                <span>
+                                    <span className="font-semibold">Titolo: </span>
+                                    {article.title}
                                 </span>
+                                <span>
+                                    <span className="font-semibold">Autore: </span>
+                                    {article.author}
+                                </span>
+                            </div>
+                            <div
+                                onClick={handleRemoveClick}
+                                className="absolute top-[-10px] right-0 text-xl text-red-600 pt-1 pr-1 cursor-pointer hover:scale-125"
+                            >
+                                x
                             </div>
                         </li>
                     ))}
